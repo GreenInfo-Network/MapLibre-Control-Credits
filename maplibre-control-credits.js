730503c0e9e48c1f0cff2f85c264c0238650ec00
Sarah Fossheim <sarah@fossheim.io>
2023-06-09 16:19:50 +0200

    remove target=_blank to open links in new tab

--- maplibre-control-credits.js
+++ maplibre-control-credits.js
@@ -11,7 +11,7 @@ class MaplibreControlCredits {
         const div = document.createElement('div');
         div.className = 'mapboxgl-ctrl mapboxgl-ctrl-group maplibre-control-credits';
 
-        div.innerHTML = `<a href="${this.options.linkUrl}" target="_blank"><img src="${this.options.imageUrl}" alt="${this.options.imageAlt}" /></a>`;
+        div.innerHTML = `<a href="${this.options.linkUrl}"><img src="${this.options.imageUrl}" alt="${this.options.imageAlt}" /></a>`;
 
         return div;
     }
