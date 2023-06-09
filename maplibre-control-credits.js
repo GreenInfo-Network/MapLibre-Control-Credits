0195b25c62476bd8184c2c735628e4f7e70970f0
Sarah Fossheim <sarah@fossheim.io>
2023-06-09 15:52:35 +0200

    add alt text to image

--- maplibre-control-credits.js
+++ maplibre-control-credits.js
@@ -3,6 +3,7 @@ class MaplibreControlCredits {
         this.options = Object.assign({
             linkUrl: "/",
             imageUrl: "//:0",
+            imageAlt: "",
         }, options);
     }
 
@@ -10,7 +11,7 @@ class MaplibreControlCredits {
         const div = document.createElement('div');
         div.className = 'mapboxgl-ctrl mapboxgl-ctrl-group maplibre-control-credits';
 
-        div.innerHTML = `<a href="${this.options.linkUrl}" target="_blank"><img src="${this.options.imageUrl}" alt="" /></a>`;
+        div.innerHTML = `<a href="${this.options.linkUrl}" target="_blank"><img src="${this.options.imageUrl}" alt="${this.options.imageAlt}" /></a>`;
 
         return div;
     }
